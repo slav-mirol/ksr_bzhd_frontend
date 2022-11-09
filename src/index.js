504c9eab51bd0431912114f526ae562e442bdd0e
slav-mirol <slavmirol@gmail.com>
2022-11-09 21:12:08 +0400

    modified:   src/index.js изменена фраза: NIKITA PIDOR на Носов Хуесос

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import * as ReactDOMClient from "react-dom/client";
 
 const elem = (<div>
-    <h1>NIKITA PIDOR</h1>
+    <h1>Носов хуесос</h1>
   </div>);
 
  const app = ReactDOMClient.createRoot(document.getElementById("root"));
