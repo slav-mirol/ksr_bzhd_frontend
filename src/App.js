69777b42eb43c232b1904c42cc1585daac1274b0
nikitanosov1 <nikinosov2002@mail.ru>
2022-11-11 21:14:41 +0400

    add three pictures

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React, { useState, useEffect } from 'react';
+import LeftPicture from './assets/leftPicture.png';
+import MidPicture from './assets/midPicture.png';
+import RightPicture from './assets/rightPicture.png';
+
 
 const App = (props) => {
     return (
       <div className="App">
+        <img src={LeftPicture} />
+        <img src={MidPicture} />
+        <img src={RightPicture} />
         <div>Тут начинаем писать уже свои теги и так далее, див с className App не трогаем</div>
       </div>
     );
