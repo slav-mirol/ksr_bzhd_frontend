4036f46f907698ac134dee0948561f1fba101e0c
nikitanosov1 <nikinosov2002@mail.ru>
2022-11-12 21:59:18 +0400

    added button

--- src/App.js
+++ src/App.js
@@ -16,6 +16,9 @@ const App = (props) => {
             <div class='mid-wrapper__title title'>а <span class='mid-wrapper__title_background-color'>чё</span> надо то?</div>
             <div class='mid-wrapper__subtitle subtitle'>/когда лень думать самому/</div>
             <div class='mid-wrapper__subsubtitle subtitle'>помощь в выборе <span class='mid-wrapper__subsubtitle_pink-color'>УЗО</span> и <span class='mid-wrapper__subsubtitle_purple-color'>автоматических выключателей</span></div>
+            <button>
+              <div class='button-text'>НАЧАТЬ</div>
+            </button>
           </section>
           <section class='right-wrapper'>
             <img class='right-picture' src={RightPicture} alt='Правая загагуля'/>
