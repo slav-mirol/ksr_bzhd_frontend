0fea718170ce9a4272f59ed8a0c6a5b55a595ced
slav-mirol <slavmirol@gmail.com>
2022-11-25 22:09:46 +0400

    modified:   src/about/About.js 	modified:   src/calculator/components/Device.js 	modified:   src/calculator/components/Line.js

--- src/calculator/components/Line.js
+++ src/calculator/components/Line.js
@@ -67,7 +67,7 @@ class Line extends React.Component {
                 }}>
                   <div className="edit-btn__icon">
                     <svg width="53" height="53" viewBox="0 0 53 53" fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <path d="M14.9062 38.0938H38.0937" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                      <path d="M14.9062 38.0938H38.0937" stroke="white" strokeWidth="2" stroke-linecap="round" stroke-linejoin="round"/>
                       <path d="M14.9062 31.4687V24.8437L30.0115 10.8174C30.2086 10.6344 30.5151 10.6401 30.7053 10.8303L35.5447 15.6697C35.7349 15.8599 35.7406 16.1664 35.5576 16.3635L21.5312 31.4687H14.9062Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                     </svg>
                   </div>
